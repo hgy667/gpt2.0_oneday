f633352dea7de2176d42776cefcb3543f476046e
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-04-16 02:55:25 +0800

    Create sm.tsx

---
+++ app/components/sm.tsx
@@ -0,0 +1,14 @@
+<SettingItem title={Locale.Settings.Lang.Name}>
+            <select
+              value={getLang()}
+              onChange={(e) => {
+                changeLang(e.target.value as any);
+              }}
+            >
+              {AllLangs.map((lang) => (
+                <option value={lang} key={lang}>
+                  {Locale.Settings.Lang.Options[lang]}
+                </option>
+              ))}
+            </select>
+          </SettingItem>
