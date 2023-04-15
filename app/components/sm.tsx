a7c2d4e560956c022dbb5a33270f18ff160610b8
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-04-16 03:01:39 +0800

    Update sm.tsx

--- app/components/sm.tsx
+++ app/components/sm.tsx
@@ -2,6 +2,37 @@ import { useState, useEffect, useMemo, HTMLProps } from "react";
 
 import EmojiPicker, { Theme as EmojiTheme } from "emoji-picker-react";
 
+import styles from "./settings.module.scss";
+
+import ResetIcon from "../icons/reload.svg";
+import CloseIcon from "../icons/close.svg";
+import ClearIcon from "../icons/clear.svg";
+import EditIcon from "../icons/edit.svg";
+import EyeIcon from "../icons/eye.svg";
+import EyeOffIcon from "../icons/eye-off.svg";
+
+import { List, ListItem, Popover, showToast } from "./ui-lib";
+
+import { IconButton } from "./button";
+import {
+  SubmitKey,
+  useChatStore,
+  Theme,
+  ALL_MODELS,
+  useUpdateStore,
+  useAccessStore,
+  ModalConfigValidator,
+} from "../store";
+import { Avatar } from "./chat";
+
+import Locale, { AllLangs, changeLang, getLang } from "../locales";
+import { getEmojiUrl } from "../utils";
+import Link from "next/link";
+import { UPDATE_URL } from "../constant";
+import { SearchService, usePromptStore } from "../store/prompt";
+import { requestUsage } from "../requests";
+import { ErrorBoundary } from "./error";
+import { InputRange } from "./input-range";
 
 <SettingItem title={Locale.Settings.Lang.Name}>
             <select
