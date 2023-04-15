20989d03b922162e852f02ea4110427e5ae99680
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-04-16 02:27:02 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -26,244 +26,241 @@ import { REPO_URL } from "../constant";
 import { ErrorBoundary } from "./error";
 
 export function Loading(props: { noLogo?: boolean }) {
-  return (
-    <div className={styles["loading-content"]}>
-      {!props.noLogo && <BotIcon />}
-      <LoadingIcon />
-    </div>
-  );
+return (
+<div className={styles["loading-content"]}>
+{!props.noLogo && <BotIcon />}
+<LoadingIcon />
+</div>
+);
 }
 
-const Settings = dynamic(async () => (await import("./settings")).Settings, {
-  loading: () => <Loading noLogo />,
-});
+const Settings = dynamic(
+async () => (await import("./settings")).Settings,
+{
+loading: () => <Loading noLogo />,
+},
+);
 
 const ChatList = dynamic(async () => (await import("./chat-list")).ChatList, {
-  loading: () => <Loading noLogo />,
+loading: () => <Loading noLogo />,
 });
 
 function useSwitchTheme() {
-  const config = useChatStore((state) => state.config);
+const config = useChatStore((state) => state.config);
 
-  useEffect(() => {
-    document.body.classList.remove("light");
-    document.body.classList.remove("dark");
+useEffect(() => {
+document.body.classList.remove("light");
+document.body.classList.remove("dark");
 
-    if (config.theme === "dark") {
-      document.body.classList.add("dark");
-    } else if (config.theme === "light") {
-      document.body.classList.add("light");
-    }
+if (config.theme === "dark") {
+  document.body.classList.add("dark");
+} else if (config.theme === "light") {
+  document.body.classList.add("light");
+}
 
-    const metaDescriptionDark = document.querySelector(
-      'meta[name="theme-color"][media]',
-    );
-    const metaDescriptionLight = document.querySelector(
-      'meta[name="theme-color"]:not([media])',
-    );
+const metaDescriptionDark = document.querySelector(
+  'meta[name="theme-color"][media]',
+);
+const metaDescriptionLight = document.querySelector(
+  'meta[name="theme-color"]:not([media])',
+);
 
-    if (config.theme === "auto") {
-      metaDescriptionDark?.setAttribute("content", "#151515");
-      metaDescriptionLight?.setAttribute("content", "#fafafa");
-    } else {
-      const themeColor = getCSSVar("--themeColor");
-      metaDescriptionDark?.setAttribute("content", themeColor);
-      metaDescriptionLight?.setAttribute("content", themeColor);
-    }
-  }, [config.theme]);
+if (config.theme === "auto") {
+  metaDescriptionDark?.setAttribute("content", "#151515");
+  metaDescriptionLight?.setAttribute("content", "#fafafa");
+} else {
+  const themeColor = getCSSVar("--themeColor");
+  metaDescriptionDark?.setAttribute("content", themeColor);
+  metaDescriptionLight?.setAttribute("content", themeColor);
+}
+}, [config.theme]);
 }
 
 function useDragSideBar() {
-  const limit = (x: number) => Math.min(500, Math.max(220, x));
+const limit = (x: number) => Math.min(500, Math.max(220, x));
 
-  const chatStore = useChatStore();
-  const startX = useRef(0);
-  const startDragWidth = useRef(chatStore.config.sidebarWidth ?? 300);
-  const lastUpdateTime = useRef(Date.now());
+const chatStore = useChatStore();
+const startX = useRef(0);
+const startDragWidth = useRef(chatStore.config.sidebarWidth ?? 300);
+const lastUpdateTime = useRef(Date.now());
 
-  const handleMouseMove = useRef((e: MouseEvent) => {
-    if (Date.now() < lastUpdateTime.current + 100) {
-      return;
-    }
-    lastUpdateTime.current = Date.now();
-    const d = e.clientX - startX.current;
-    const nextWidth = limit(startDragWidth.current + d);
-    chatStore.updateConfig((config) => (config.sidebarWidth = nextWidth));
-  });
+const handleMouseMove = useRef((e: MouseEvent) => {
+if (Date.now() < lastUpdateTime.current + 100) {
+return;
+}
+lastUpdateTime.current = Date.now();
+const d = e.clientX - startX.current;
+const nextWidth = limit(startDragWidth.current + d);
+chatStore.updateConfig((config) => (config.sidebarWidth = nextWidth));
+});
 
-  const handleMouseUp = useRef(() => {
-    startDragWidth.current = chatStore.config.sidebarWidth ?? 300;
-    window.removeEventListener("mousemove", handleMouseMove.current);
-    window.removeEventListener("mouseup", handleMouseUp.current);
-  });
+const handleMouseUp = useRef(() => {
+startDragWidth.current = chatStore.config.sidebarWidth ?? 300;
+window.removeEventListener("mousemove", handleMouseMove.current);
+window.removeEventListener("mouseup", handleMouseUp.current);
+});
 
-  const onDragMouseDown = (e: MouseEvent) => {
-    startX.current = e.clientX;
+const onDragMouseDown = (e: MouseEvent) => {
+startX.current = e.clientX;
 
-    window.addEventListener("mousemove", handleMouseMove.current);
-    window.addEventListener("mouseup", handleMouseUp.current);
-  };
+window.addEventListener("mousemove", handleMouseMove.current);
+window.addEventListener("mouseup", handleMouseUp.current);
+};
 
-  useEffect(() => {
-    if (isMobileScreen()) {
-      return;
-    }
+useEffect(() => {
+if (isMobileScreen()) {
+return;
+}
 
-    document.documentElement.style.setProperty(
-      "--sidebar-width",
-      `${limit(chatStore.config.sidebarWidth ?? 300)}px`,
-    );
-  }, [chatStore.config.sidebarWidth]);
+document.documentElement.style.setProperty(
+  "--sidebar-width",
+  `${limit(chatStore.config.sidebarWidth ?? 300)}px`,
+);
+}, [chatStore.config.sidebarWidth]);
 
-  return {
-    onDragMouseDown,
-  };
+return {
+onDragMouseDown,
+};
 }
 
 const useHasHydrated = () => {
-  const [hasHydrated, setHasHydrated] = useState<boolean>(false);
+const [hasHydrated, setHasHydrated] = useState<boolean>(false);
 
-  useEffect(() => {
-    setHasHydrated(true);
-  }, []);
+useEffect(() => {
+setHasHydrated(true);
+}, []);
 
-  return hasHydrated;
+return hasHydrated;
 };
 
 function _Home() {
-  const [createNewSession, currentIndex, removeSession] = useChatStore(
-    (state) => [
-      state.newSession,
-      state.currentSessionIndex,
-      state.removeSession,
-    ],
-  );
-  const chatStore = useChatStore();
-  const loading = !useHasHydrated();
-  const [showSideBar, setShowSideBar] = useState(true);
+const [createNewSession, currentIndex, removeSession] = useChatStore(
+(state) => [
+state.newSession,
+state.currentSessionIndex,
+state.removeSession,
+],
+);
+const chatStore = useChatStore();
+const loading = !useHasHydrated();
+const [showSideBar, setShowSideBar] = useState(true);
 
-  // setting
-  const [openSettings, setOpenSettings] = useState(false);
-  const config = useChatStore((state) => state.config);
+// setting
+const [openSettings, setOpenSettings] = useState(false);
+const config = useChatStore((state) => state.config);
 
-  // drag side bar
-  const { onDragMouseDown } = useDragSideBar();
+// drag side bar
+const { onDragMouseDown } = useDragSideBar();
 
-  useSwitchTheme();
+useSwitchTheme();
 
-  if (loading) {
-    return <Loading />;
-  }
+if (loading) {
+return <Loading />;
+}
 
-  return (
+return (
+<div
+className={${config.tightBorder && !isMobileScreen() ? styles["tight-container"] : styles.container         }}
+>
+<div className={styles.sidebar + ${showSideBar && styles["sidebar-show"]}}>
+<div className={styles["sidebar-header"]}>
+<div className={styles["sidebar-title"]}>ChatGPT 独众传媒 v2.0</div>
+<div className={styles["sidebar-sub-title"]}>
+<p>
+- 本网站基于ChatGPT和Github开源项目制作
+</p>
+<p>
+- 仅供独众传媒内部使用 切勿外传 避免网站被封！！！
+</p>
+<p>
+- 本网站由何高阳开发与维护，未经允许不得用于商业用途
+</p>
+<p>
+- 在使用过程中遇到任何问题可以联系本人
+</p>
+</div>
+<div className={styles["sidebar-logo"]}>
+<ChatGptIcon />
+</div>
+</div>
+
     <div
-      className={`${
-        config.tightBorder && !isMobileScreen()
-          ? styles["tight-container"]
-          : styles.container
-      }`}
+      className={styles["sidebar-body"]}
+      onClick={() => {
+        setOpenSettings(false);
+        setShowSideBar(false);
+      }}
     >
-      <div
-        className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
-      >
-        <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT 独众传媒 v2.0</div>
-          <div className={styles["sidebar-sub-title"]}>
-            <p>
-            - 本网站基于ChatGPT和Github开源项目制作 
-              </p>
-            <p>
-            - 仅供独众传媒内部使用 切勿外传 避免网站被封！！！
-              </p>
-            <p>
-            - 本网站由何高阳开发与维护，未经允许不得用于商业用途
-              </p>
-            <p>
-            - 在使用过程中遇到任何问题可以联系本人
-              </p>
-          </div>
-          <div className={styles["sidebar-logo"]}>
-            <ChatGptIcon />
-          </div>
-        </div>
+      <ChatList />
+    </div>
 
-        <div
-          className={styles["sidebar-body"]}
+    <div className={styles["sidebar-tail"]}>
+      <div className={styles["sidebar-actions"]}>
+        <div className={styles["sidebar-action"] + " " + styles.mobile}>
+          <IconButton
+            icon={<CloseIcon />}
+            onClick={chatStore.deleteSession}
+          />
+        </div>
+        <div className={styles["sidebar-action"]}>
+          <IconButton
+            icon={<SettingsIcon />}
+            onClick={() => {
+              setOpenSettings(true);
+              setShowSideBar(false);
+            }}
+            shadow
+          />
+        </div>
+        <div className={styles["sidebar-action"]}>
+          <a href="http://www.dzhongmedia.com/" target="_blank">
+            <IconButton icon={<GithubIcon />} shadow />
+          </a>
+        </div>
+      </div>
+      <div>
+        <IconButton
+          icon={<AddIcon />}
+          text={Locale.Home.NewChat}
           onClick={() => {
-            setOpenSettings(false);
+            createNewSession();
             setShowSideBar(false);
           }}
-        >
-          <ChatList />
-        </div>
-
-        <div className={styles["sidebar-tail"]}>
-          <div className={styles["sidebar-actions"]}>
-            <div className={styles["sidebar-action"] + " " + styles.mobile}>
-              <IconButton
-                icon={<CloseIcon />}
-                onClick={chatStore.deleteSession}
-              />
-            </div>
-            <div className={styles["sidebar-action"]}>
-              <IconButton
-                icon={<SettingsIcon />}
-                onClick={() => {
-                  setOpenSettings(true);
-                  setShowSideBar(false);
-                }}
-                shadow
-              />
-            </div>
-            <div className={styles["sidebar-action"]}>
-              <a href="http://www.dzhongmedia.com/" target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
-              </a>
-            </div>
-          </div>
-          <div>
-            <IconButton
-              icon={<AddIcon />}
-              text={Locale.Home.NewChat}
-              onClick={() => {
-                createNewSession();
-                setShowSideBar(false);
-              }}
-              shadow
-            />
-          </div>
-        </div>
-
-        <div
-          className={styles["sidebar-drag"]}
-          onMouseDown={(e) => onDragMouseDown(e as any)}
-        ></div>
-      </div>
-
-      <div className={styles["window-content"]}>
-        {openSettings ? (
-          <Settings
-            closeSettings={() => {
-              setOpenSettings(false);
-              setShowSideBar(true);
-            }}
-          />
-        ) : (
-          <Chat
-            key="chat"
-            showSideBar={() => setShowSideBar(true)}
-            sideBarShowing={showSideBar}
-          />
-        )}
+          shadow
+        />
       </div>
     </div>
-  );
+
+    <div
+      className={styles["sidebar-drag"]}
+      onMouseDown={(e) => onDragMouseDown(e as any)}
+    ></div>
+  </div>
+
+  <div className={styles["window-content"]}>
+    {openSettings ? (
+      <Settings
+        closeSettings={() => {
+          setOpenSettings(false);
+          setShowSideBar(true);
+        }}
+      />
+    ) : (
+      <Chat
+        key="chat"
+        showSideBar={() => setShowSideBar(true)}
+        sideBarShowing={showSideBar}
+      />
+    )}
+  </div>
+</div>
+);
 }
 
 export function Home() {
-  return (
-    <ErrorBoundary>
-      <_Home></_Home>
-    </ErrorBoundary>
-  );
+return (
+<ErrorBoundary>
+<_Home></_Home>
+</ErrorBoundary>
+);
 }
