f388a8d4df27a250a6e9942a77dc0f9f6a3dcd7e
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-04-16 03:00:02 +0800

    Update sm.tsx

--- app/components/sm.tsx
+++ app/components/sm.tsx
@@ -1,3 +1,8 @@
+import { useState, useEffect, useMemo, HTMLProps } from "react";
+
+import EmojiPicker, { Theme as EmojiTheme } from "emoji-picker-react";
+
+
 <SettingItem title={Locale.Settings.Lang.Name}>
             <select
               value={getLang()}
