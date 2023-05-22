c7472aba0242ea3368640bd6b4304bd28feac970
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-05-22 18:44:21 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,17 +25,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         
-<>
-  {/* Google tag (gtag.js) */}
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-9TL6NDC1P9"></script>
-  <script>
-    {`window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FJMVFJ0RT5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    gtag('config', 'G-9TL6NDC1P9');`}
-  </script>
-</>
+  gtag('config', 'G-FJMVFJ0RT5');
+</script>
         
         
         <meta
